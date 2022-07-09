9cc0cd3e35d114a540a8bfaca73b859e58bbf6fa
sasanzare <sasanzare75@gmail.com>
2022-07-09 22:10:44 +0430

    Initial commit

---
+++ wild life/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wild life
+//
+//  Created by sasan on 7/9/22.
+//  Https://sasanzare.ir
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("wild life")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wild life/wild_lifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  wild_lifeApp.swift
+//  wild life
+//
+//  Created by sasan on 7/9/22.
+//  Https://sasanzare.ir
+
+import SwiftUI
+
+@main
+struct wild_lifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
